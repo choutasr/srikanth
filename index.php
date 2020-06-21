87c97fa60b0b2dee2440baf8a285896467c0a36d
choutasr <35955013+choutasr@users.noreply.github.com>
2020-06-21 18:30:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
 echo "Hello World! and this file is from pep app server in south east asia and this is for revanth to see";
+
+ 
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
