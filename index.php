8762a7a4e615135e6dd9bdf90f301e09fc3e5483
choutasr <35955013+choutasr@users.noreply.github.com>
2020-06-21 18:32:11 +0530

    test

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello World! and this file is from pep app server in south east asia and this is for revanth to see";
+echo "Hello World! and this file is from pep app server in south east asia";
 
  
 echo "Today is " . date("Y/m/d") . "<br>";
