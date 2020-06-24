f6909f8b25e79780db85889edf4c9161075dc4c6
choutasr <35955013+choutasr@users.noreply.github.com>
2020-06-24 18:35:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello World! and this file is from pep app server in south east asia";
+echo "Hello World! and this file is from pep app server in south east asia and end to end SSL". "<br>";
 
  
 echo "Today is " . date("Y/m/d") . "<br>";
