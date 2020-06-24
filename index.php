fea36e4b8329716e76a763664ab0a061bf9868bf
choutasr <35955013+choutasr@users.noreply.github.com>
2020-06-24 18:42:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,4 @@ echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l") . "<br>" ;
-echo "--------------------"
+echo "--------------------";
